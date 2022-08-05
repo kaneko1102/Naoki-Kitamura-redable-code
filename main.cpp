980394616759515275672923889002a3ad61645a
Naoki Kitamura <seikei135do@gmail.com>
2022-08-05 13:11:32 +0900

    spec5: add ID to word

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <vector>
 
 #define DICTIONARY_PATH "./dictionary-data.txt"
 
@@ -8,8 +9,14 @@ int main(void){
     std::ifstream inputStream(DICTIONARY_PATH);
     std::string buff;
 
+    std::vector<std::string> wordLIst;
+
     while(getline(inputStream, buff)){
-        std::cout << buff << std::endl;
+        wordLIst.push_back(buff);
+    }
+
+    for(int i=0;i<wordLIst.size();i++){
+        std::cout << i << ": " << wordLIst[i] << std::endl;
     }
 
     // ƒRƒ“ƒ\[ƒ‹‚ª‘¦•Â‚¶‚é‚Ì‚ð–h‚®‚½‚ß‚É“ü—Í‘Ò‚¿
